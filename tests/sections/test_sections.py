7793e80a501dcae5afa2137ae15b2e9ef05978ea
Stephen Moore <stephen@delfick.com>
2016-09-19 11:20:25 +1000

    Finish test

--- tests/sections/test_sections.py
+++ tests/sections/test_sections.py
@@ -145,6 +145,8 @@ describe TestCase, "Sections":
 
             it "can combine with a date":
                 res = self.parser.time_spec_to_object("time(hour: 16, minute:20) & date(month:3, day:10, year:1980)")
+                self.assertEqual(type(res), final.DateTimeSpec)
+                self.assertEqual(res.datetime.strftime("%s"), "321517200")
 
         describe "sunrise":
             it "does not simplify":
