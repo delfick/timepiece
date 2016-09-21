a15f05b3872df651af8e0161e3a88f8939c9502b
Stephen Moore <stephen@delfick.com>
2016-09-21 11:45:49 +1000

    Fixing make_timepiece

--- timepiece/spec.py
+++ timepiece/spec.py
@@ -1,7 +1,12 @@
-from timepiece.sections.base import JoinerSpec, SectionSpec
 from timepiece.grammar import TimeSpecGrammar, TimeSpecVisitor
+from timepiece.sections.base import JoinerSpec, SectionSpec
+from timepiece.sections import sections as sections_module
 
 from delfick_error import DelfickError
+
+# Make vim quiet
+# only imported so sections are registered
+sections_module = sections_module
 
 def make_timepiece(ErrorKls=DelfickError, JoinerSpec=JoinerSpec, SectionSpec=SectionSpec, sections=None):
 	Joiner = type("Joiner", (JoinerSpec, ), {"ErrorKls": ErrorKls})
