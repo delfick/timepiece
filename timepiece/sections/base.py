5fb06ebf1f0f31beada4e1061c91209989ec3607
Stephen Moore <stephen@delfick.com>
2017-05-22 14:08:47 +1000

    Fix default available sections for SectionSpec

--- timepiece/sections/base.py
+++ timepiece/sections/base.py
@@ -82,7 +82,10 @@ class SectionSpec(sb.Spec):
     ErrorKls = DelfickError
 
     def setup(self, available_sections=None):
-        self.available_sections = available_sections
+        if available_sections is None:
+            self.available_sections = default_available_sections
+        else:
+            self.available_sections = available_sections
 
     def normalise_filled(self, meta, val):
         if isinstance(val, dictobj):
